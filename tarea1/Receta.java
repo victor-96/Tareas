22e6b58a6ae995be30bc5320d9afd545c812f1eb
victor <victor1996d@live.com>
2017-05-20 18:12:42 +0200

    He generado los getter y setter
    Signed-off-by: victor <victor1996d@live.com>

--- tarea1/Receta.java
+++ tarea1/Receta.java
@@ -24,6 +24,30 @@ public class Receta {
         this.ingredientes = ingredientes;
         this.preparacion = preparacion;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getPreparacion() {
+        return preparacion;
+    }
+
+    public void setPreparacion(String preparacion) {
+        this.preparacion = preparacion;
+    }
     
     
 }
